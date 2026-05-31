89e8aecbeacee57ebdf011ae49a6f32910bb0a8b
MarkCshandreOngkit <markongkit001@gmail.com>
2026-05-31 16:39:48 +0800

    updated functions to schedule, no css yet

--- dashboard.js
+++ dashboard.js
@@ -3,7 +3,7 @@ const schedDate = document.getElementById("sched-date-label");
 const schedRoom = document.getElementById("sched-room-label");
 const selectedWeekLabel = document.getElementById("calendar-selected-date");
 //current time
-const today = new Date()
+const today = new Date();
 
 const weekDay = today.toLocaleDateString("en-US", {
   weekday: "short"
@@ -31,48 +31,150 @@ window.addEventListener("DOMContentLoaded", () => {
   if (defaultWeekButton) defaultWeekButton.classList.add("active");
   if (defaultViewStateButton) defaultViewStateButton.classList.add("active");
 
+  renderAll();
   updateWeekButtons();
   schedDateLabelRefresh();
 });
 
+function renderAll() {
+  renderRoomSelection();
+};
+
 //booking-form
 
-//room-selection
-const floors = {
-    floor1: [101, 102, 103, 104],
-    floor2: [201, 202, 203, 204],
-    floor3: [301, 302, 303, 304],
-    floor4: [401, 402, 403, 404],
-}
+function createSelectionState() {
+  let selectedFloor = null;
 
-const floorSelect = document.getElementById("booking-floor")
-const roomSelect = document.getElementById("booking-room")
+  return {
+    getFloor: () => selectedFloor,
+    setFloor: (value) => {selectedFloor = value;}
+  };
+};
+
+const state = createSelectionState();
+
+//room-selection
+const floors = [
+  {
+    id: "floor1",
+    rooms: []
+  },
+  {
+    id: "floor2",
+    rooms: []
+  },
+  {
+    id: "floor3",
+    rooms: []
+  },
+  {
+    id: "floor4",
+    rooms: []
+  },
+];
+/* data structure needed
+  room is the dic in rooms so something like rooms: [{}, {}, {}]
+  room = {
+    roomNum: 101,
+    timeSlots: [
+      {
+        start: "7:30 AM",
+        end: "10:30 AM",
+        status: "available", "occupied", "tentative", "unavailable" //these are options
+        reservedBy: None,
+        purpose: None
+      }
+      {etc.}
+    ]
+  }
+*/
+let floorCount = 0;
+
+floors.forEach(floor => {
+  floorCount++;
+
+  for (let i = 0; i < 5; i++) {
+    floor.rooms.push({
+      roomNum: `${floorCount}0${i}`,
+      timeSlots: []
+    })
+  }
+
+  floor.rooms.forEach(room => {
+    const defaultSlotStartTime = "7:30 AM";
+    let startTime = defaultSlotStartTime;
+    let endTime = minutesToTime(timeToMinutes(startTime) + 180) //3 hrs
+    for (let i = 0; i < 4; i++) {
+      room.timeSlots.push({
+      start: startTime,
+      end: endTime,
+      status: "available",
+      reservedBy: null,
+      purpose: null
+    })
+    startTime = endTime;
+    endTime = minutesToTime(timeToMinutes(startTime) + 180)
+    }
+  })
+});
+
+const floorSelect = document.getElementById("booking-floor");
+const roomSelect = document.getElementById("booking-room");
+
 
 floorSelect.addEventListener("change", function () {
-  const selectedFloor = this.value;
+  state.setFloor(this.value);
 
-  roomSelect.innerHTML = '<option value="" selected disabled hidden>Select Room</option>';
-
-  if (floors[selectedFloor]) {
-    floors[selectedFloor].forEach(item => {
-      const option = document.createElement("option");
-      option.value = item;
-      option.textContent = item;
-      roomSelect.appendChild(option);
-    });
-  }
-  const defaultRoom = "###"
-  schedRoomLabelRefresh(defaultRoom);
+  renderRoomOptions();
 });
 
 roomSelect.addEventListener("change", function () {
   const selectedRoom = this.value;
   schedRoomLabelRefresh(selectedRoom);
-})
+  
+  const floor = floors.find(f => f.id === state.getFloor());
+  const room = floor.rooms.find(r => r.roomNum === selectedRoom)
+  console.log(room.timeSlots)
+  renderSlots(room.timeSlots)
+});
+
+function renderFloorOptions() {
+  floorSelect.innerHTML = `<option value="" selected disabled hidden>Select Floor</option>`
+  floors.forEach(floor => {
+    const floorOption = document.createElement("option");
+    floorOption.value = floor.id;
+    let floorNum = toOrdinal(Number(floor.id.replace("floor", "")))
+    floorOption.textContent = floorNum + " Floor";
+    floorSelect.appendChild(floorOption);
+  })
+  state.setFloor(null)
+};
+
+function renderRoomOptions() {
+  roomSelect.innerHTML = '<option value="" selected disabled hidden>Select Room</option>';
+
+  const floor = floors.find(f => f.id === state.getFloor());
+
+  if (floor) {
+    floor.rooms.forEach(room => {
+      const roomOption = document.createElement("option");
+      roomOption.value = room.roomNum;
+      roomOption.textContent = room.roomNum;
+      roomSelect.appendChild(roomOption);
+    });
+  }
+  const defaultRoom = "###"
+  schedRoomLabelRefresh(defaultRoom);
+};
+
+function renderRoomSelection() {
+  renderFloorOptions();
+  renderRoomOptions();
+};
 
 //additional-notes
-const textarea = document.getElementById("add-notes")
-const counter = document.getElementById("counter")
+const textarea = document.getElementById("add-notes");
+const counter = document.getElementById("counter");
 
 textarea.addEventListener("input", function () {
     counter.textContent = this.value.length + " / 500 characters"
@@ -93,8 +195,8 @@ bookingForm.addEventListener("submit", function (event) {
 //live-calendar-mainframe
 
 //calendar-navbar
-const prevBtn = document.getElementById("calendar-prev-btn")
-const nextBtn = document.getElementById("calendar-next-btn")
+const prevBtn = document.getElementById("calendar-prev-btn");
+const nextBtn = document.getElementById("calendar-next-btn");
 
 prevBtn.addEventListener("click", () => {
   weekOffset--;
@@ -125,6 +227,7 @@ weekButtons.forEach(btn => {
       weekButtons.forEach(b => b.classList.remove("active"));
       btn.classList.add("active");
       schedDateLabelRefresh();
+      renderAll();
     });
 });
 
@@ -158,19 +261,19 @@ function updateWeekButtons() {
     btn.dataset.date = currentDate.toISOString();
 
     selectedWeekLabel.textContent = `${firstDayLabel} - ${lastDayLabel}`;
-})}
+})};
 
 function getActiveWeekButton () {
   return Array.from(weekButtons).find(btn =>
     btn.classList.contains("active")
   );
-}
+};
 
 function getActiveWeekDate () {
   const activeBtn = getActiveWeekButton();
   if (!activeBtn) return null;
   return new Date(activeBtn.dataset.date);
-}
+};
 
 //schedule-navbar-btns
 const viewStateButtons = document.querySelectorAll(".view-state-buttons");
@@ -194,14 +297,79 @@ function schedDateLabelRefresh() {
     year: "numeric"
   });
   schedDate.textContent = "Schedule for " + formattedSchedDate
-}
+};
 
-function schedRoomLabelRefresh(selectedRoom) {
-  schedRoom.textContent = "Room " + selectedRoom
-}
+function schedRoomLabelRefresh(roomNum) {
+  schedRoom.textContent = "Room " + roomNum
+};
 
 //schedule-graph
+function renderSlots(roomTimeSlots) {
+  const grid = document.getElementById("schedule-grid")
+
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+
+  roomTimeSlots.forEach(timeSlot => {
+    const roomStartTime = timeSlot.start
+    const roomEndTime = timeSlot.end
+    const roomStatus = timeSlot.status
+    const slot = document.createElement("div")
+    slot.value = `${roomStartTime} - ${roomEndTime}`;
+    slot.textContent = `${roomStartTime} - ${roomEndTime}`;
+    slot.classList.add(`${roomStatus}-slot`)
+    grid.appendChild(slot);
+  })
+};
+
+//utility functions
+function toOrdinal(num) {
+  let suffix = "th";
+
+  if ((num % 10) === 1 && (num % 100) !== 11) {
+    suffix = "st";
+  }
+  else if ((num % 10) === 2 && (num % 100) !== 12) {
+    suffix = "nd";
+  }
+  else if ((num % 10) === 3 && (num % 100) !== 13) {
+    suffix = "rd";
+  }
+
+  return num + suffix;
+};
+
 function timeToMinutes(time) {
-  const [h, m] = time.split(":").map(Number);
-  return h * 60 + m;
-} //still not finished
+  let [hours, rest] = time.split(":");
+  let [minutes, period] = rest.trim().split(" ");
+
+  hours = Number(hours);
+  minutes = Number(minutes);
+
+  if (period === "PM" && hours !== 12) {
+    hours += 12;
+  }
+  else if (period === "AM" && hours === 12) {
+    hours = 0;
+  }
+  return hours * 60 + minutes;
+};
+
+function minutesToTime(minutes) {
+  let h = Math.floor(minutes / 60);
+  let m = minutes % 60;
+  
+  let period = "AM";
+  
+  if (h >= 12) {
+    period = "PM";
+  }
+
+  h = h % 12; 
+  if (h === 0) {h = 12}; // edge case for 0:00 midnight, not needed but just there
+
+  m = String(m).padStart(2, "0");
+
+  return `${h}:${m} ${period}`;
+};
